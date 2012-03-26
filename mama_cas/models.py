2b41d4954e5bddbca4f4767779747e83f6d20355
Jason Bittel <jason.bittel@gmail.com>
2012-03-25 22:50:08 -0700

    Move LoginTicket ticket generation into LoginTicket save method

--- mama_cas/models.py
+++ mama_cas/models.py
@@ -21,11 +21,12 @@ class Ticket(models.Model):
         self.created_on = datetime.now()
         super(Ticket, self).save(*args, **kwargs)
 
-    def generate_ticket_id(self, prefix=''):
+    def generate_ticket(self, prefix=''):
         """
         Create a sufficiently opaque ticket string using random data
         to ensure a ``Ticket`` is not guessable. An optional prefix string
         can be provided that is prepended to the ticket string.
+
         """
         self.ticket = "%s-%d-%s" % (prefix, int(time.time()), hashlib.sha1(os.urandom(512)).hexdigest())
 
@@ -34,25 +35,41 @@ class Ticket(models.Model):
         A ``Ticket`` is consumed by when it is used by populating the
         ``consumed`` field with the current datetime. A consumed ``Ticket``
         is no longer valid for any future authentication attempts.
+
         """
         self.consumed = datetime.now()
 
 class LoginTicketManager(models.Manager):
     """
     Create a new ``LoginTicket``, populate ``ticket`` with a new ticket
-    id and save the ``LoginTicket`` to the database. Return the generated
-    ticket ID.
+    string and save the ``LoginTicket`` to the database. Return the
+    generated ticket ID.
+
     """
     def create_login_ticket(self):
-        lt = LoginTicket()
-        lt.generate_ticket_id(prefix='LT')
-        lt.save()
-
-        return lt.ticket
+        login_ticket = LoginTicket()
+        login_ticket.save()
+        return login_ticket.ticket
 
 class LoginTicket(Ticket):
+    """
+    A string that is saved to the DB and passed to /login as a credential
+    acceptor to prevent replaying of credentials. A ``LoginTicket`` is
+    created automatically when the /login form is displayed.
+
+
+    You shouldn't need to interact directly with this model. Instead,
+    ``LoginTicketManager`` provides methods for creating, validating
+    and deleting expired ``LoginTicket``s.
+
+    """
+    TICKET_PREFIX = u"LT"
+
     objects = LoginTicketManager()
 
+    def save(self, *args, **kwargs):
+        self.generate_ticket(prefix=TICKET_PREFIX)
+        super(LoginTicket, self).save(*args, **kwargs)
 
 class ServiceTicket(Ticket):
     service = models.CharField(max_length=256)
