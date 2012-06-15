e2f70bfc583d6b8b1bd656bcd935e289342366f7
Jason Bittel <jason.bittel@gmail.com>
2012-06-15 16:09:32 -0700

    Add initial setup script

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+
+setup(
+    name='django-mama-cas',
+    version='0.1',
+    description='A CAS server single sign-on application for Django',
+    author='Jason Bittel',
+    author_email='jason.bittel@gmail.com',
+    packages=['mama_cas']
+)
