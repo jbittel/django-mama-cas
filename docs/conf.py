4b0a32d66e7d5f4fe012d1c21d45335d0571d629
Jason Bittel <jason.bittel@gmail.com>
2013-05-08 15:52:23 -0700

    Tweak docs configuration

--- docs/conf.py
+++ docs/conf.py
@@ -25,10 +25,10 @@ sys.path.insert(0, os.path.abspath('..'))
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc']
+extensions = []
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+#templates_path = ['_templates']
 
 # The suffix of source filenames.
 source_suffix = '.rst'
