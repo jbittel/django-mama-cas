6e447b2f85c265806d9f4452239482c68178c874
Jason Bittel <jason.bittel@gmail.com>
2013-05-10 09:23:40 -0700

    Greatly simplify setup file

--- setup.py
+++ setup.py
@@ -1,79 +1,49 @@
+#!/usr/bin/env python
+
 from distutils.core import setup
 import os
 
 from mama_cas import __version__ as version
 
 
-def split_relative_path(path):
+def read_file(filename):
     """
-    Given a path, return the path as a string with the
-    first path component removed (e.g. 'foo/bar/baz' would
-    be returned as 'bar/baz').
-    """
-    parts = []
-    while True:
-        head, tail = os.path.split(path)
-        if head == path:
-            if path:
-                parts.append(path)
-            break
-        parts.append(tail)
-        path = head
-    parts.reverse()
-    if len(parts) > 1:
-        return os.path.join(*parts[1:])
-    else:
-        return ''
-
-
-def get_readme(filename):
-    """
-    Utility function to print the README file, used for the long_description
-    setup argument below.
+    Utility function to read a provided filename.
     """
     return open(os.path.join(os.path.dirname(__file__), filename)).read()
 
 
-packages, package_data = [], []
-root_dir = os.path.dirname(__file__)
-if root_dir:
-    os.chdir(root_dir)
+packages = [
+    'mama_cas',
+    'mama_cas.management',
+    'mama_cas.management.commands',
+    'mama_cas.tests'
+]
 
-# Collect the lists of packages and package files, starting
-# from the base project directory (adapted from the Django setup script)
-for dirpath, dirnames, filenames in os.walk('mama_cas'):
-    # Collect packages
-    if '__init__.py' in filenames:
-        pkg_path = os.path.normpath(dirpath)
-        pkg = pkg_path.replace(os.sep, '.')
-        if os.altsep:
-            pkg = pkg.replace(os.altsep, '.')
-        packages.append(pkg)
-    # Collect ancillary package files
-    elif filenames:
-        relative_path = split_relative_path(dirpath)
-        for f in filenames:
-            package_data.append(os.path.join(relative_path, f))
+package_data = {
+    '': ['LICENSE', 'README.rst'],
+    'mama_cas': ['templates/mama_cas/*.html', 'templates/mama_cas/*.xml']
+}
 
 setup(
     name='django-mama-cas',
     version=version,
     description='A CAS server single sign-on application for Django',
-    long_description=get_readme('README.rst'),
+    long_description=read_file('README.rst'),
     author='Jason Bittel',
     author_email='jason.bittel@gmail.com',
     url='http://github.com/jbittel/django-mama-cas',
     download_url='http://github.com/jbittel/django-mama-cas/downloads',
     package_dir={'mama_cas': 'mama_cas'},
     packages=packages,
-    package_data={'mama_cas': package_data},
+    package_data=package_data,
     license='BSD',
     classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Web Environment',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.6'
-        'Programming Language :: Python :: 2.7'
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Framework :: Django',
