2db0cab8c9eebcb6f5a029fa1bf93d118b9089d8
Jason Bittel <jason.bittel@gmail.com>
2014-08-01 09:26:16 -0700

    Add middleware classes to test settings
    Fixes test failures under Django 1.7c2

--- mama_cas/tests/settings.py
+++ mama_cas/tests/settings.py
@@ -19,6 +19,14 @@ PASSWORD_HASHERS = (
     'django.contrib.auth.hashers.MD5PasswordHasher',
 )
 
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+)
+
 INSTALLED_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
