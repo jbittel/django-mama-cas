47b6a282a92084181d2daea3cda09fd1745496b5
Jason Bittel <jason.bittel@gmail.com>
2015-03-12 14:42:11 -0700

    Fix user foreign keys in South migrations

--- mama_cas/south_migrations/0001_initial.py
+++ mama_cas/south_migrations/0001_initial.py
@@ -5,6 +5,17 @@ from south.v2 import SchemaMigration
 from django.db import models
 
 
+try:
+    from django.contrib.auth import get_user_model
+except ImportError:
+    from django.contrib.auth.models import User
+else:
+    User = get_user_model()
+
+user_app_object = '%s.%s' % (User._meta.app_label, User._meta.object_name)
+user_app_module = u'%s.%s' % (User._meta.app_label, User._meta.module_name)
+
+
 class Migration(SchemaMigration):
 
     def forwards(self, orm):
@@ -12,7 +23,7 @@ class Migration(SchemaMigration):
         db.create_table(u'mama_cas_serviceticket', (
             (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
             ('ticket', self.gf('django.db.models.fields.CharField')(unique=True, max_length=255)),
-            ('user', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['profile.UserProfile'])),
+            ('user', self.gf('django.db.models.fields.related.ForeignKey')(to=orm[user_app_object])),
             ('expires', self.gf('django.db.models.fields.DateTimeField')()),
             ('consumed', self.gf('django.db.models.fields.DateTimeField')(null=True)),
             ('service', self.gf('django.db.models.fields.CharField')(max_length=255)),
@@ -24,7 +35,7 @@ class Migration(SchemaMigration):
         db.create_table(u'mama_cas_proxyticket', (
             (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
             ('ticket', self.gf('django.db.models.fields.CharField')(unique=True, max_length=255)),
-            ('user', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['profile.UserProfile'])),
+            ('user', self.gf('django.db.models.fields.related.ForeignKey')(to=orm[user_app_object])),
             ('expires', self.gf('django.db.models.fields.DateTimeField')()),
             ('consumed', self.gf('django.db.models.fields.DateTimeField')(null=True)),
             ('service', self.gf('django.db.models.fields.CharField')(max_length=255)),
@@ -36,7 +47,7 @@ class Migration(SchemaMigration):
         db.create_table(u'mama_cas_proxygrantingticket', (
             (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
             ('ticket', self.gf('django.db.models.fields.CharField')(unique=True, max_length=255)),
-            ('user', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['profile.UserProfile'])),
+            ('user', self.gf('django.db.models.fields.related.ForeignKey')(to=orm[user_app_object])),
             ('expires', self.gf('django.db.models.fields.DateTimeField')()),
             ('consumed', self.gf('django.db.models.fields.DateTimeField')(null=True)),
             ('iou', self.gf('django.db.models.fields.CharField')(unique=True, max_length=255)),
@@ -58,6 +69,42 @@ class Migration(SchemaMigration):
 
 
     models = {
+        u'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        u'auth.permission': {
+            'Meta': {'ordering': "(u'content_type__app_label', u'content_type__model', u'codename')", 'unique_together': "((u'content_type', u'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['contenttypes.ContentType']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        user_app_module: {
+            'Meta': {'object_name': User.__name__, 'db_table': "'%s'" % User._meta.db_table},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "u'user_set'", 'blank': 'True', 'to': u"orm['auth.Group']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "u'user_set'", 'blank': 'True', 'to': u"orm['auth.Permission']"}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        u'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
         u'mama_cas.proxygrantingticket': {
             'Meta': {'object_name': 'ProxyGrantingTicket'},
             'consumed': ('django.db.models.fields.DateTimeField', [], {'null': 'True'}),
@@ -67,7 +114,7 @@ class Migration(SchemaMigration):
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'iou': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '255'}),
             'ticket': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '255'}),
-            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['profile.UserProfile']"})
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['%s']" % user_app_object})
         },
         u'mama_cas.proxyticket': {
             'Meta': {'object_name': 'ProxyTicket'},
@@ -77,7 +124,7 @@ class Migration(SchemaMigration):
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'service': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
             'ticket': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '255'}),
-            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['profile.UserProfile']"})
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['%s']" % user_app_object})
         },
         u'mama_cas.serviceticket': {
             'Meta': {'object_name': 'ServiceTicket'},
@@ -87,21 +134,7 @@ class Migration(SchemaMigration):
             'primary': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
             'service': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
             'ticket': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '255'}),
-            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['profile.UserProfile']"})
-        },
-        u'profile.userprofile': {
-            'Meta': {'object_name': 'UserProfile'},
-            'auth_backend': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'}),
-            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
-            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'id_number': ('django.db.models.fields.CharField', [], {'max_length': '9', 'blank': 'True'}),
-            'ip_phone': ('django.db.models.fields.CharField', [], {'max_length': '9', 'blank': 'True'}),
-            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
-            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
-            'manager': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'}),
-            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
-            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '255'})
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['%s']" % user_app_object})
         }
     }
 
