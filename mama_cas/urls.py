340ef8d93ba1324c713e60ad19e7f0c92df6f7e7
Jason Bittel <jason.bittel@gmail.com>
2015-12-09 18:21:20 -0800

    Remove patterns from URLconf

--- mama_cas/urls.py
+++ mama_cas/urls.py
@@ -2,7 +2,6 @@
 (2) CAS server URIs as described in the CAS protocol.
 """
 
-from django.conf.urls import patterns
 from django.conf.urls import url
 
 from mama_cas.views import LoginView
@@ -15,35 +14,15 @@ from mama_cas.views import WarnView
 from mama_cas.views import SamlValidateView
 
 
-urlpatterns = patterns('',
-    url(r'^login/?$',
-        LoginView.as_view(),
-        name='cas_login'),
-    url(r'^logout/?$',
-        LogoutView.as_view(),
-        name='cas_logout'),
-    url(r'^validate/?$',
-        ValidateView.as_view(),
-        name='cas_validate'),
-    url(r'^serviceValidate/?$',
-        ServiceValidateView.as_view(),
-        name='cas_service_validate'),
-    url(r'^proxyValidate/?$',
-        ProxyValidateView.as_view(),
-        name='cas_proxy_validate'),
-    url(r'^proxy/?$',
-        ProxyView.as_view(),
-        name='cas_proxy'),
-    url(r'^p3/serviceValidate/?$',
-        ServiceValidateView.as_view(),
-        name='cas_p3_service_validate'),
-    url(r'^p3/proxyValidate/?$',
-        ProxyValidateView.as_view(),
-        name='cas_p3_proxy_validate'),
-    url(r'^warn/?$',
-        WarnView.as_view(),
-        name='cas_warn'),
-    url(r'^samlValidate/?$',
-        SamlValidateView.as_view(),
-        name='cas_saml_validate'),
-)
+urlpatterns = [
+    url(r'^login/?$', LoginView.as_view(), name='cas_login'),
+    url(r'^logout/?$', LogoutView.as_view(), name='cas_logout'),
+    url(r'^validate/?$', ValidateView.as_view(), name='cas_validate'),
+    url(r'^serviceValidate/?$', ServiceValidateView.as_view(), name='cas_service_validate'),
+    url(r'^proxyValidate/?$', ProxyValidateView.as_view(), name='cas_proxy_validate'),
+    url(r'^proxy/?$', ProxyView.as_view(), name='cas_proxy'),
+    url(r'^p3/serviceValidate/?$', ServiceValidateView.as_view(), name='cas_p3_service_validate'),
+    url(r'^p3/proxyValidate/?$', ProxyValidateView.as_view(), name='cas_p3_proxy_validate'),
+    url(r'^warn/?$', WarnView.as_view(), name='cas_warn'),
+    url(r'^samlValidate/?$', SamlValidateView.as_view(), name='cas_saml_validate'),
+]
