ffb42ba8e9b0a5d7a269ee9d13a5347f4ffee563
Jason Bittel <jason.bittel@gmail.com>
2016-02-03 07:58:18 -0800

    Test short_name for user name attributes callback

--- mama_cas/tests/test_callbacks.py
+++ mama_cas/tests/test_callbacks.py
@@ -6,26 +6,24 @@ from mama_cas.callbacks import user_name_attributes
 
 
 class CallbacksTests(TestCase):
-    url = 'http://www.example.com/'
-
     def setUp(self):
         self.user = UserFactory()
 
-    def test_user_name(self):
+    def test_user_name_attributes(self):
         """
-        The callback should return a username and full_name
-        attribute.
+        The callback should return a username, full_name and
+        short_name attribute.
         """
-        attributes = user_name_attributes(self.user, self.url)
+        attributes = user_name_attributes(self.user, 'http://www.example.com/')
         self.assertIn('username', attributes)
         self.assertEqual(attributes['username'], 'ellen')
         self.assertIn('full_name', attributes)
         self.assertEqual(attributes['full_name'], 'Ellen Cohen')
+        self.assertIn('short_name', attributes)
+        self.assertEqual(attributes['short_name'], 'Ellen')
 
     def test_user_model_attributes(self):
-        """
-        The callback should return at least a username attribute.
-        """
-        attributes = user_model_attributes(self.user, self.url)
+        """The callback should return at least a username attribute."""
+        attributes = user_model_attributes(self.user, 'http://www.example.com/')
         self.assertIn('username', attributes)
         self.assertEqual(attributes['username'], 'ellen')
