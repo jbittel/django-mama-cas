d16238db18eca5bd76183c6c54ef060da6ff44bc
BarnabasSzabolcs <szabolcs.barnabas@gmail.com>
2019-10-25 18:38:34 +0200

    Makes SSL certification validation configurable
    If MAMA_CAS_VERIFY_SSL_CERTIFICATE == False then SSL certificates are not verified. This configuration is useful for localdev with self-signed certificates (eg. generated by mkcert).

--- mama_cas/models.py
+++ mama_cas/models.py
@@ -214,6 +214,7 @@ class ServiceTicketManager(TicketManager):
         be sent. Otherwise, synchronous requests will be sent.
         """
         session = Session()
+        session.verify = getattr(settings, 'MAMA_CAS_VERIFY_SSL_CERTIFICATE', True)
         for ticket in self.filter(user=user, consumed__gte=user.last_login):
             ticket.request_sign_out(session=session)
 
