500ec2744b384933657b52c22b9c22f6a2c0e373
Santos Gallegos <stsewd@protonmail.com>
2020-10-23 08:54:22 -0500

    Python 3.8

--- setup.py
+++ setup.py
@@ -33,6 +33,7 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Topic :: System :: Systems Administration :: Authentication/Directory',
     ],
